1b62c89fd2f520bc13118854bdc4d9f10ac11a58
Alfredo <alfredofontinele08@gmail.com>
2022-05-22 12:37:45 -0300

    update

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,34 @@
+// Funcionalidade do Botão do Áudio
+
+let count = 0;
+let btn_music = document.querySelector("button");
+const audio = document.querySelector("audio");
+
+function ativarAudio() {
+    if (count == 0) {
+        count = 1;
+        audio.currentTime = 0.1;
+        audio.autoplay = true;
+        audio.loop = true;
+        audio.play();
+        btn_music.innerHTML = "Desativar Musica";
+    } else {
+        count = 0;
+        audio.pause();
+        btn_music.innerHTML = "Ativar Musica";
+    }
+}
+
+// Funcionalidade do Menu Mobile
+
 const navbar = document.querySelector(".header_right");
 const button = document.querySelector(".menu_mobile");
 
 button.addEventListener("click", () => {
     navbar.classList.toggle("active");
 })
+
+// Funcionalidade do Botão Mudar Tema de Fundo
 
 function mudarTema() {
     let main = document.querySelector("main");
@@ -15,3 +40,24 @@ function mudarTema() {
         main.classList.add("main1");
     }
 }
+
+
+// Testes de Audio
+
+// let audio = document.querySelector("audio");
+
+// async function ativaAudio() {
+//     try {
+//         audio.autoplay = true;
+//         audio.loop = true;
+//         await audio.play();
+//     }
+//     catch (error) {
+//         console.log(error);
+//     }
+//     finally {
+//         await audio.play();
+//     }
+// }
+
+// window.onload = ativaAudio();
